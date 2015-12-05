e9dfa097d83cbbb3c4fa8cd6ce9c4a9fa6805ee9
Craig Drayton <craig.drayton@gmail.com>
2015-12-05 02:07:22 +0000

    Styles map, adds placeholder navbar

--- client/templates/map/foodmap.js
+++ client/templates/map/foodmap.js
@@ -4,6 +4,17 @@ if (Meteor.isClient) {
   });
 }
 
+Template.foodmap.onCreated(function() {
+  // We can use the `ready` callback to interact with the map API once the map is ready.
+  GoogleMaps.ready('exampleMap', function(map) {
+    // Add a marker to the map once it's ready
+    var marker = new google.maps.Marker({
+      position: map.options.center,
+      map: map.instance
+    });
+  });
+});
+
 Template.foodmap.helpers({
   exampleMapOptions: function() {
     // Make sure the maps API has loaded
