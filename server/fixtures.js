f209549f727e978ad5389a9922d9863681d1b868
Craig Drayton <craig.drayton@gmail.com>
2015-12-05 05:29:14 +0000

    adds accounts, users in fixture, login buttons in navbar

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,12 +1,32 @@
-if (Listings.find().count() === 0) {
+if (Meteor.users.find().count() === 0) {
+
+    var bobId = Accounts.createUser({
+        'username'  : 'Bob Bobson',
+        'email'     : 'bob@bob.com',
+        'password'  : 'bobbob' //encrypted automatically
+    });
+
+    var bob = Meteor.users.findOne(bobId);
+
+    var sueId = Accounts.createUser({
+        'username'  : 'Sue Susson',
+        'email'     : 'sue@sue.com',
+        'password'  : 'suesue' //encrypted automatically
+    });
+
+    var sue = Meteor.users.findOne(sueId);
+
+
     Listings.insert({
+        creatorId: bobId,
         location: {
             position: {lat: -37.830636, lng: 145.051237},
             map: 'foodMap'
         }
     });
-    
+
     Listings.insert({
+        creatorId: sueId,
         location: {
             position: {lat: -37.831369, lng: 145.057717},
             map: 'foodMap'
